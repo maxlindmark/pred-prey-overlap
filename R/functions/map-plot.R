6bf29497872744452feb073017f8226dbb0f93ae
Max Lindmark <max.lindmark@slu.se>
2023-12-07 14:09:36 +0100

    update mapplot

--- R/functions/map-plot.R
+++ R/functions/map-plot.R
@@ -53,41 +53,18 @@ plot_map <-
   xlim(xmin2, xmax2) +
   ylim(ymin2, ymax2) +
   labs(x = "Longitude", y = "Latitude") +
-  geom_sf(size = 0.3) + 
+  geom_sf(size = 0.3, color = "gray80") + 
   theme_sleek() +
   guides(colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
          fill = guide_colorbar(title.position = "top", title.hjust = 0.5)) +
   NULL
-
-# This is for the diet data
-# xmin3 <- 303000
-# xmax3 <- 916000
-# xrange <- xmax2 - xmin3
-# 
-# ymin3 <- 5980000
-# ymax3 <- 6580000
-# yrange <- ymax3 - ymin3
-# 
-# plot_map_d <- 
-#   ggplot(swe_coast_proj) + 
-#   xlim(xmin3*1.5, xmax3*0.9) +
-#   ylim(ymin3*1.025, ymax3*0.98268) + 
-#   labs(x = "Longitude", y = "Latitude") +
-#   geom_sf(size = 0.3) + 
-#   theme_sleek() +
-#   guides(colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
-#          fill = guide_colorbar(title.position = "top", title.hjust = 0.5)) +
-#   NULL
-
-# small_cod_stomach <- read_csv("data/clean/small_cod_stomach.csv") 
-# plot_map_d + geom_point(data = small_cod_stomach, aes(X*1000, Y*1000))
 
 plot_map_fc <- 
   ggplot(swe_coast_proj) + 
   xlim(xmin2, xmax2) +
   ylim(ymin2, ymax2) +
   labs(x = "Longitude", y = "Latitude") +
-  geom_sf(size = 0.3) + 
+  geom_sf(size = 0.3, color = "gray80") + 
   theme_facet_map() +
   guides(colour = guide_colorbar(title.position = "top", title.hjust = 0.5),
          fill = guide_colorbar(title.position = "top", title.hjust = 0.5)) +
@@ -95,22 +72,22 @@ plot_map_fc <-
 
 plot_map_labels <- 
   plot_map + 
-  annotate("text", label = "Sweden", x = xmin2 + 0.25*xrange, y = ymin2 + 0.75*yrange, color = "black", size = 1.9) +
-  annotate("text", label = "Denmark", x = xmin2 + 0.029*xrange, y = ymin2 + 0.32*yrange, color = "black", size = 1.9, angle = 75) +
-  annotate("text", label = "Germany", x = xmin2 + 0.07*xrange, y = ymin2 + 0.022*yrange, color = "black", size = 1.9) +
-  annotate("text", label = "Poland", x = xmin2 + 0.55*xrange, y = ymin2 + 0.08*yrange, color = "black", size = 1.9) +
-  annotate("text", label = "Russia", x = xmin2 + 0.95*xrange, y = ymin2 + 0.18*yrange, color = "black", size = 1.9) +
-  annotate("text", label = "Lithuania", x = xmin2 + 1*xrange, y = ymin2 + 0.43*yrange, color = "black", size = 1.9, angle = 75) +
-  annotate("text", label = "Latvia", x = xmin2 + 0.99*xrange, y = ymin2 + 0.65*yrange, color = "black", size = 1.9, angle = 75)
+  annotate("text", label = "Sweden", x = xmin2 + 0.25*xrange, y = ymin2 + 0.75*yrange, color = "gray40", size = 1.9) +
+  annotate("text", label = "Denmark", x = xmin2 + 0.029*xrange, y = ymin2 + 0.32*yrange, color = "gray40", size = 1.9, angle = 75) +
+  annotate("text", label = "Germany", x = xmin2 + 0.07*xrange, y = ymin2 + 0.022*yrange, color = "gray40", size = 1.9) +
+  annotate("text", label = "Poland", x = xmin2 + 0.55*xrange, y = ymin2 + 0.08*yrange, color = "gray40", size = 1.9) +
+  annotate("text", label = "Russia", x = xmin2 + 0.95*xrange, y = ymin2 + 0.18*yrange, color = "gray40", size = 1.9) +
+  annotate("text", label = "Lithuania", x = xmin2 + 1*xrange, y = ymin2 + 0.43*yrange, color = "gray40", size = 1.9, angle = 75) +
+  annotate("text", label = "Latvia", x = xmin2 + 0.99*xrange, y = ymin2 + 0.65*yrange, color = "gray40", size = 1.9, angle = 75)
 
 plot_map_labels_fc <- 
   plot_map_fc + 
-  annotate("text", label = "Sweden", x = xmin2 + 0.25*xrange, y = ymin2 + 0.75*yrange, color = "black", size = 1.9) +
-  annotate("text", label = "Denmark", x = xmin2 + 0.029*xrange, y = ymin2 + 0.32*yrange, color = "black", size = 1.9, angle = 75) +
-  annotate("text", label = "Germany", x = xmin2 + 0.07*xrange, y = ymin2 + 0.022*yrange, color = "black", size = 1.9) +
-  annotate("text", label = "Poland", x = xmin2 + 0.55*xrange, y = ymin2 + 0.08*yrange, color = "black", size = 1.9) +
-  annotate("text", label = "Russia", x = xmin2 + 0.95*xrange, y = ymin2 + 0.18*yrange, color = "black", size = 1.9) +
-  annotate("text", label = "Lithuania", x = xmin2 + 1*xrange, y = ymin2 + 0.43*yrange, color = "black", size = 1.9, angle = 75) +
-  annotate("text", label = "Latvia", x = xmin2 + 0.99*xrange, y = ymin2 + 0.65*yrange, color = "black", size = 1.9, angle = 75)
+  annotate("text", label = "Sweden", x = xmin2 + 0.25*xrange, y = ymin2 + 0.75*yrange, color = "gray40", size = 1.9) +
+  annotate("text", label = "Denmark", x = xmin2 + 0.029*xrange, y = ymin2 + 0.32*yrange, color = "gray40", size = 1.9, angle = 75) +
+  annotate("text", label = "Germany", x = xmin2 + 0.07*xrange, y = ymin2 + 0.022*yrange, color = "gray40", size = 1.9) +
+  annotate("text", label = "Poland", x = xmin2 + 0.55*xrange, y = ymin2 + 0.08*yrange, color = "gray40", size = 1.9) +
+  annotate("text", label = "Russia", x = xmin2 + 0.95*xrange, y = ymin2 + 0.18*yrange, color = "gray40", size = 1.9) +
+  annotate("text", label = "Lithuania", x = xmin2 + 1*xrange, y = ymin2 + 0.43*yrange, color = "gray40", size = 1.9, angle = 75) +
+  annotate("text", label = "Latvia", x = xmin2 + 0.99*xrange, y = ymin2 + 0.65*yrange, color = "gray40", size = 1.9, angle = 75)
 
 # Diet map plot here!
